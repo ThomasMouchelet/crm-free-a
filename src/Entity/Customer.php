eacb51501d6c5718470a3151a1bdb2e0f244f343
ThomasMouchelet <mouchelet.thomas@gmail.com>
2020-12-18 08:27:56 +0100

    init

---
+++ src/Entity/Customer.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CustomerRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity(repositoryClass=CustomerRepository::class)
+ * @ApiResource(
+ *          normalizationContext={
+ *              "groups"={"customers_read"}
+ *          }
+ * )
+ */
+class Customer
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @Groups({"customers_read"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"customers_read"})
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"customers_read"})
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"customers_read"})
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"customers_read"})
+     */
+    private $company;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Invoice::class, mappedBy="customer")
+     * @Groups({"customers_read"})
+     */
+    private $invoices;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="customers")
+     * @Groups({"customers_read"})
+     */
+    private $freelancer;
+
+    public function __construct()
+    {
+        $this->invoices = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCompany(): ?string
+    {
+        return $this->company;
+    }
+
+    public function setCompany(string $company): self
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Invoice[]
+     */
+    public function getInvoices(): Collection
+    {
+        return $this->invoices;
+    }
+
+    public function addInvoice(Invoice $invoice): self
+    {
+        if (!$this->invoices->contains($invoice)) {
+            $this->invoices[] = $invoice;
+            $invoice->setCustomer($this);
+        }
+
+        return $this;
+    }
+
+    public function removeInvoice(Invoice $invoice): self
+    {
+        if ($this->invoices->removeElement($invoice)) {
+            // set the owning side to null (unless already changed)
+            if ($invoice->getCustomer() === $this) {
+                $invoice->setCustomer(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getFreelancer(): ?User
+    {
+        return $this->freelancer;
+    }
+
+    public function setFreelancer(?User $freelancer): self
+    {
+        $this->freelancer = $freelancer;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Invoice.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InvoiceRepository;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity(repositoryClass=InvoiceRepository::class)
+ * @ApiResource(
+ *          normalizationContext={
+ *              "groups"={"invoices_read"}
+ *          }
+ * )
+ */
+class Invoice
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @Groups({"invoices_read"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="float")
+     * @Groups({"invoices_read"})
+     */
+    private $amount;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Groups({"invoices_read"})
+     */
+    private $sendingAt;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"invoices_read"})
+     */
+    private $status;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Customer::class, inversedBy="invoices")
+     * @Groups({"invoices_read"})
+     */
+    private $customer;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAmount(): ?float
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(float $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getSendingAt(): ?\DateTimeInterface
+    {
+        return $this->sendingAt;
+    }
+
+    public function setSendingAt(\DateTimeInterface $sendingAt): self
+    {
+        $this->sendingAt = $sendingAt;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCustomer(): ?Customer
+    {
+        return $this->customer;
+    }
+
+    public function setCustomer(?Customer $customer): self
+    {
+        $this->customer = $customer;
+
+        return $this;
+    }
+}
